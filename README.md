376ae7597d1848ef62b708edb2ce8d0963c72c12
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2024-03-12 07:30:34 -0600

    Performing the Deployment

---
+++ README.md
@@ -0,0 +1,36 @@
+# GeoWeather
+
+![GeoWeather Banner](public/readme-banner.png)
+
+## Features ✨
+
+- Autocomplete search
+- Map
+- Current day weather data
+- Current day data by hour
+- Forecast day weather
+- Forecast day data by hour
+- Celsius / Farenheit
+- Temperature Chart
+- Dark Mode
+
+## Installation 🔧
+
+Install GeoWeather
+
+- Go to the project folder
+- Run the following commands
+
+```bash
+  npm install
+  npm run dev
+```
+
+- Go to `index.html`
+- Replace the API Key from Google Maps API with your key, available [here](https://mapsplatform.google.com/intl/es-419_ALL/)
+
+```
+<script async
+   src="https://maps.googleapis.com/maps/api/js?key={YOURAPIKEY}&language=en&components=locality&libraries=places&callback=myCallbackFunc">
+</script>
+```
---
+++ src/components/HeaderFooter/Footer.jsx
@@ -0,0 +1,40 @@
+export default function Footer() {
+  return (
+    <div className='min-h-[15vh] bg-gray-100 dark:bg-gray-900 grid p-5 text-gray-700 dark:text-white grid-cols-1 grid-rows-3 md:grid-rows-1 md:grid-cols-3 place-items-center items-center '>
+      <a
+        className='font-bold'
+        target='_blank'
+        href='portafolio-alan1.netlify.app'
+        rel='noreferrer'
+      >
+        Portfolio.dev
+      </a>
+      <div className='grid place-items-center'>
+        <p>
+          Made with ❤️ &nbsp;by{' '}
+          <a
+            target='_blank'
+            href='https://www.linkedin.com/in/alan-rosete-m-20ba03216'
+            rel='noreferrer'
+          >
+            Alan Rosete
+          </a>
+        </p>
+        <div className='flex'>
+          Icons by&nbsp;
+          <a target='_ blank' href='https://icons8.com' rel='noreferrer'>
+            Icons8
+          </a>
+        </div>
+      </div>
+      <a
+        className='font-bold'
+        target='_blank'
+        href='https://www.github.com/AlanRosete'
+        rel='noreferrer'
+      >
+        Github
+      </a>
+    </div>
+  )
+}
