0810e3d7fc25fc8a14f48b52d0e02cc68e8fbb64
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2024-07-18 18:12:36 -0600

    Add Details In GeoWeather

--- src/components/Utils/SearchBar.jsx
+++ src/components/Utils/SearchBar.jsx
@@ -1,16 +1,31 @@
 import { useContext, useState } from 'react'
-
 import PlacesAutocomplete from 'react-places-autocomplete/dist/PlacesAutocomplete'
 import { WeatherContext } from '../../context/WeatherContextProvider'
+import { COUNTRIES_AND_CITIES } from '../Const'
 
 export default function SearchBar() {
   const [input, setInput] = useState('')
   const { searchWeather } = useContext(WeatherContext)
+
+  const filteredCountries = COUNTRIES_AND_CITIES.filter(country =>
+    country.toLowerCase().includes(input.toLowerCase())
+  )
+
+  const handleCountryClick = (country) => {
+    setInput(country)
+    searchWeather({ newAddress: country })
+    setInput("")
+  }
+
   return (
     <form className='relative z-10 grid grid-cols-2 '>
       <PlacesAutocomplete
-        onChange={(address) => setInput(address)}
+        onChange={(address) => {
+          setInput(address)
+          console.log(filteredCountries)
+        }}
         value={input}
+
         onSelect={(newAddress, placeId, suggestion) => {
           searchWeather({ newAddress })
           setInput('')
@@ -18,7 +33,7 @@ export default function SearchBar() {
         googleCallbackName='initOne'
       >
         {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div className=' w-full col-span-3 md:col-end-4 md:col-span-2'>
+          <div className='w-full col-span-3 md:col-end-4 md:col-span-2'>
             <input
               {...getInputProps({
                 placeholder: 'Search Places ...',
@@ -26,16 +41,17 @@ export default function SearchBar() {
                   'w-full h-10 p-3 mt-5 border rounded border-cardGray place-self-end'
               })}
             />
-            <div className='absolute z-50 max-w-xs p-3 bg-white border rounded top-15 h-fit empty:hidden border-cardGray place-self-start'>
+            <div className='absolute z-50 p-3 bg-white border rounded top-15 h-fit empty:hidden border-cardGray place-self-start w-6/12'>
               {loading && ''}
+
               {suggestions.map((suggestion) => {
                 const className = suggestion.active
                   ? 'mt-2 border-b border-solid border-cardGray'
                   : 'mt-2 border-b border-dashed border-cardGray'
-                // inline style for demonstration purpose
                 const style = suggestion.active
                   ? { backgroundColor: '#fafafa', cursor: 'pointer' }
                   : { backgroundColor: '#ffffff', cursor: 'pointer' }
+
                 return (
                   <div
                     {...getSuggestionItemProps(suggestion, {
@@ -48,6 +64,16 @@ export default function SearchBar() {
                   </div>
                 )
               })}
+              {input.length > 1 && filteredCountries.map((country, index) => (
+                <div
+                  key={index}
+                  style={{ cursor: 'pointer' }}
+                  className='min-w-full'
+                  onClick={() => handleCountryClick(country) }
+                >
+                  <span>{country}</span>
+                </div>
+              ))}
             </div>
           </div>
         )}
