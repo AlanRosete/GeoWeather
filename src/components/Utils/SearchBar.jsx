6800c7b29a789a9fbf49ef8dcf2bca5d58856df8
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2024-07-18 18:17:41 -0600

    Add Fix Duplicate IN Countries

--- src/components/Utils/SearchBar.jsx
+++ src/components/Utils/SearchBar.jsx
@@ -2,7 +2,6 @@ import { useContext, useState } from 'react'
 import PlacesAutocomplete from 'react-places-autocomplete/dist/PlacesAutocomplete'
 import { WeatherContext } from '../../context/WeatherContextProvider'
 import { COUNTRIES_AND_CITIES } from '../Const'
-import { COUNTRIES_AND_CITIES } from '../Const'
 
 export default function SearchBar() {
   const [input, setInput] = useState('')
